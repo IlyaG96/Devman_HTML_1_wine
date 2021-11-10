088b2de23f11386659028aec530f711447c09840
IlyaG96 <ilyag_96@icloud.com>
2021-11-10 16:17:58 +1100

    upload project to git

---
+++ main.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from render import time_of_life, make_wine_database, find_min_price
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html'])
+)
+template = env.get_template('template.html')
+
+
+rendered_page = template.render(
+    time_of_life=time_of_life(),
+    wine_database=make_wine_database(),
+    min_price=find_min_price()
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
---
+++ render.py
@@ -0,0 +1,33 @@
+from datetime import date
+from collections import defaultdict
+import pandas
+
+
+def time_of_life() -> str:
+    """returns the age of the winery"""
+    time = (date.today().year - 1920)
+    if str(time).endswith("2" or "3"):
+        return f"{time} года"
+    elif str(time).endswith("1"):
+        return f"{time} год"
+    else:
+        return f"{time} лет"
+
+
+def make_wine_database() -> dict:
+    """returns a dictionary containing information about wines and drinks"""
+    wine_database = defaultdict(list)
+    wine_descriptions = pandas.read_excel('wine3.xlsx', sheet_name="Лист1", keep_default_na=False)
+    wine_descriptions = wine_descriptions.sort_index()
+    wine_list = wine_descriptions.to_dict("records")
+    for i in range(len(wine_list)):
+        wine_database[wine_list[i]["Категория"]].append(wine_list[i])
+    return dict(wine_database)
+
+
+def find_min_price() -> int:
+    """returns minimal price of vine"""
+    wine_descriptions = pandas.read_excel('wine3.xlsx', sheet_name="Лист1", keep_default_na=False)
+    min_price = min(wine_descriptions["Цена"].to_list())
+    return min_price
+
