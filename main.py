61b99955ae62b6d358d4d572b9cbe529ec3cd7be
IlyaG96 <ilyag_96@icloud.com>
2021-11-11 11:36:34 +1100

    reformat docstrings

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from render import count_time_if_life, make_wine_database, find_min_price
+from render import count_winery_age, make_wine_database, find_min_price
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -10,7 +10,7 @@ template = env.get_template('template.html')
 
 
 rendered_page = template.render(
-    time_of_life=count_time_if_life(),
+    time_of_life=count_winery_age(),
     wine_database=make_wine_database(),
     min_price=find_min_price()
 )
--- render.py
+++ render.py
@@ -3,20 +3,29 @@ from collections import defaultdict
 import pandas
 
 
-def count_time_if_life() -> str:
-    """returns the age of the winery"""
+def count_winery_age() -> str:
+    """
+    calculates the lifetime of the winery
+
+    :return f"{time} лет"
+
+    """
     year_of_foundation = 1920
-    time = date.today().year - year_of_foundation
-    if str(time).endswith("2" or "3"):
-        return f"{time} года"
-    elif str(time).endswith("1"):
-        return f"{time} год"
+    winery_age = date.today().year - year_of_foundation
+    if str(winery_age).endswith("2" or "3"):
+        return f"{winery_age} года"
+    elif str(winery_age).endswith("1"):
+        return f"{winery_age} год"
     else:
-        return f"{time} лет"
+        return f"{winery_age} лет"
 
 
 def make_wine_database() -> dict:
-    """returns a dictionary containing information about wines and drinks"""
+    """
+    returns a dictionary containing information about wines and drinks from wine.xslx using pandas
+
+    :return dict
+    """
     wine_database = defaultdict(list)
     wine_descriptions = pandas.read_excel('wine3.xlsx', sheet_name="Лист1", keep_default_na=False)
     wine_descriptions = wine_descriptions.sort_index()
@@ -27,7 +36,9 @@ def make_wine_database() -> dict:
 
 
 def find_min_price() -> int:
-    """returns minimal price of vine"""
+    """
+    returns minimal price of vine
+    """
     wine_descriptions = pandas.read_excel('wine3.xlsx', sheet_name="Лист1", keep_default_na=False)
     min_price = min(wine_descriptions["Цена"].to_list())
     return min_price
