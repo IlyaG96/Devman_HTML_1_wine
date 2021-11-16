8922d7afd65f0652234e7efb10617caec8ab588e
IlyaG96 <ilyag_96@icloud.com>
2021-11-16 22:44:06 +1100

    refactor global consts into global vars

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-
-
-
 def main():
 
     load_dotenv()
 
-    YEAR_OF_FOUNDATION = int(os.getenv("YEAR_OF_FOUNDATION", default=1920))
-    PATH_TO_DATABASE = os.getenv("PATH_TO_DATABASE", default='./wine.xlsx')
+    year_of_foundation = int(os.getenv("YEAR_OF_FOUNDATION", default=1920))
+    path_to_database = os.getenv("PATH_TO_DATABASE", default='./wine.xlsx')
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -22,8 +19,8 @@ def main():
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        time_of_life=count_winery_age(YEAR_OF_FOUNDATION),
-        wine_database=make_wine_database(PATH_TO_DATABASE),
+        time_of_life=count_winery_age(year_of_foundation),
+        wine_database=make_wine_database(path_to_database),
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
--- render.py
+++ render.py
@@ -3,14 +3,14 @@ from datetime import date
 from collections import defaultdict
 
 
-def count_winery_age(YEAR_OF_FOUNDATION: int) -> str:
+def count_winery_age(year_of_foundation: int) -> str:
     """
     calculates the lifetime of the winery
 
     :return f"{time} лет"
 
     """
-    winery_age = date.today().year - YEAR_OF_FOUNDATION
+    winery_age = date.today().year - year_of_foundation
     if str(winery_age).endswith("2" or "3"):
         return f"{winery_age} года"
     elif str(winery_age).endswith("1"):
@@ -19,14 +19,14 @@ def count_winery_age(YEAR_OF_FOUNDATION: int) -> str:
         return f"{winery_age} лет"
 
 
-def make_wine_database(PATH_TO_DATABASE: str) -> dict:
+def make_wine_database(path_to_database: str) -> dict:
     """
     returns a dictionary containing information about wines and drinks from wine.xslx using pandas
 
     :return dict
     """
     wine_database = defaultdict(list)
-    wine_descriptions = pandas.read_excel(PATH_TO_DATABASE, sheet_name="Лист1", keep_default_na=False)
+    wine_descriptions = pandas.read_excel(path_to_database, sheet_name="Лист1", keep_default_na=False)
     wine_descriptions = wine_descriptions.sort_index()
     wines = wine_descriptions.to_dict("records")
     for wine_num, wine in enumerate(wines):
